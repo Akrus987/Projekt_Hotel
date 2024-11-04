710cec40984be441b6b081cdf384144ac049c894
ppiotr9 <p_piotr9@o2.pl>
2024-11-04 17:12:40 +0100

    Update pokoj.h

--- pokoj.h
+++ pokoj.h
@@ -6,14 +6,14 @@ using namespace std;
 class Pokoj
 {
     public:
-    Pokoj();
-    Pokoj(int nr, int os);
-    int get_max(); //zwraca max liczbe osob
-    int get_numer(); //zwraca nr pokoju
-    string get_stan(); //zwraca stan
-    void zmien_stan(string nowy_stan); //zmienia stan pokoju
+        Pokoj();
+        Pokoj(int nr, int os);
+        int get_max(); //zwraca max liczbe osob
+        int get_numer(); //zwraca nr pokoju
+        string get_stan(); //zwraca stan
+        void zmien_stan(string nowy_stan); //zmienia stan pokoju
     private:
-    int numer_; //nr pokoju
-    int max_os_; //maksymalna liczba osob w pokoju
-    string stan_; //"czysty", "zajety", "do posprzatania"
+        int numer_; //nr pokoju
+        int max_os_; //maksymalna liczba osob w pokoju
+        string stan_; //"czysty", "zajety", "do posprzatania"
 };
