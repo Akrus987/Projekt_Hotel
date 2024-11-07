54402df5d0e2ca3cc655fd6db0efaa451888eed6
bartassswal <bartosz.walczak@student.put.poznan.pl>
2024-11-07 15:48:40 +0100

    Update pokoj.h

--- pokoj.h
+++ pokoj.h
@@ -14,10 +14,12 @@ class Pokoj
         int get_numer(); //zwraca nr pokoju
         string get_stan(); //zwraca stan
         void zmien_stan(string nowy_stan); //zmienia stan pokoju
+        bool czy_zarezerwowany(int dzien, int miesiac) const;
 
         friend class Rezerwacja;
     private:
         int numer_; //nr pokoju
         int max_os_; //maksymalna liczba osob w pokoju
         string stan_; //"czysty", "zajety", "do posprzatania"
+        vector<pair<pair<int, int>, Klient*>> rezerwacje_;  // rezerwacje pokoju
 };
