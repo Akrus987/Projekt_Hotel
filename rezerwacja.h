a7bd0c51af377da92e5c9ec20af0c78c959abefa
bartassswal <bartosz.walczak@student.put.poznan.pl>
2024-11-07 15:48:57 +0100

    Update rezerwacja.h

--- rezerwacja.h
+++ rezerwacja.h
@@ -2,7 +2,6 @@
 #include "data.h"
 #include "pokoj.h"
 #include "klient.h"
-#include <iostream>
 
 class Rezerwacja{
     private:
@@ -11,12 +10,17 @@ class Rezerwacja{
         Pokoj& pokoj;
         int dzien_start; // pierwszy dzien rezerwacji
         int dzien_koniec; // ostatni dzien rezerwacji
+        int miesiac_start; // miesiac w ktorym rozpoczela sie rezerwacja
+        int miesiac_koniec; // miesiac w ktorym zakonczyla sie rezerwacja
+        int rok_rezerwacji; // rok w ktorym jest rezerwacja
 
     public:
-        Rezerwacja(Data& d, Klient& k, Pokoj& p);
-        void rezerwuj(int start, int koniec); // rezerwuje pobyt od start do koniec
-        void przedluz(int nowy_koniec);  //przedluza pobyt do dnia nowy_koniec
-        void skroc(int nowy_koniec);    //skraca pobyt do dnia nowy_koniec
+        Rezerwacja(Data& d, Klient& k, Pokoj& , int start_dzien, int start_miesiac, int koniec_dzien, int koniec_miesiac, int rok_rezerwacji);
+        void rezerwuj(int start_dzien, int start_miesiac, int koniec_dzien, int koniec_miesiac, int rok); // rezerwuje pobyt od start do koniec
+        void przedluz(int nowy_koniec_dzien, int nowy_koniec_miesiac);  //przedluza pobyt do dnia nowy_koniec
+        void skroc(int nowy_koniec_dzien, int nowy_koniec_miesiac);    //skraca pobyt do dnia nowy_koniec
         void odwolaj();     //anuluje pobyt
 
 };
+
+
