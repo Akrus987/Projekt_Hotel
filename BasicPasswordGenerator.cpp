2e909a19b4b03d1c48f7e48e17d67851c5a3f6e1
Akrus987 <jakubkruzinski12@gmail.com>
2025-01-11 19:35:13 +0100

    A bit of a weird improvement to the password gen

--- BasicPasswordGenerator.cpp
+++ BasicPasswordGenerator.cpp
@@ -7,31 +7,36 @@
 #include <sstream>
 #include <stdexcept>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
+
 string GeneratePassword(string IDx)
 {
-    ifstream inFile;
-    string x;
-    string ID = IDx;
-    string name;
-    string surname;
-    inFile.open("testing_ID.txt");
-    
-    while (inFile) 
+    ifstream inFile("testing_ID.txt");
+    if (!inFile) {
+        cerr << "Unable to open file testing_ID.txt" << endl;
+        exit(1); // terminate with error
+    }
+
+    string x, ID = IDx, name, surname;
+    while (inFile >> x) 
     {
-        inFile >> x;
         if (x == ID)
         {
+            inFile >> name >> surname;
+            string newPassword, generated;
             
-            inFile >> name;
-            inFile >> surname;
-            string newPassword;
-            stringstream stream;
-            string generated;
-            stream << rand() % 501;
-            stream >> generated;
-            newPassword = name + surname + generated;
-           return newPassword;
+            // Generate a more advanced random sequence
+            srand(time(0));
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+            for (int i = 0; i < 8; ++i) {
+                generated += characters[rand() % characters.length()];
+            }
+            
+            // Mix name and surname with the generated sequence
+            newPassword = name.substr(0, 2) + surname.substr(0, 2) + generated + name.substr(2) + surname.substr(2);
+            inFile.close();
+            return newPassword;
         }
     }
     inFile.close();
@@ -40,9 +45,15 @@ string GeneratePassword(string IDx)
 
 int main()
 {
-    string pass;
-    pass = GeneratePassword("ID007");
-    cout << pass;
+    string ID;
+    cout << "Enter the ID to find (e.g., ID003): ";
+    cin >> ID;
+    
+    string pass = GeneratePassword(ID);
+    if (pass == "No ID under that index") {
+        cout << pass << endl;
+    } else {
+        cout << "Generated Password: " << pass << endl;
+    }
     return 0;
 }
-
